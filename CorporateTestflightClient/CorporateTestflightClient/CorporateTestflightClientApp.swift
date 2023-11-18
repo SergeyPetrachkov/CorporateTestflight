592fde7fb39d73d400852fa5855548016c16650b
Sergey Petrachkov <petrachkovsergey@gmail.com>
2023-11-18 09:59:01 +0100

    add a workspace

---
+++ CorporateTestflightClient/CorporateTestflightClient/CorporateTestflightClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  CorporateTestflightClientApp.swift
+//  CorporateTestflightClient
+//
+//  Created by Sergey Petrachkov on 18.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct CorporateTestflightClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
