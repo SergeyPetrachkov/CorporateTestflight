fa0185ec3f5b9752dc24c5c0192ab893c3036cdc
Sergey Petrachkov <petrachkovsergey@gmail.com>
2024-01-16 14:21:57 +0100

    fix a bug where once failed ticket task would make it impossible to retry fetching the ticket details

--- CorporateTestflightClient/CorporateTestflightClient/Data/TicketsCacheActor.swift
+++ CorporateTestflightClient/CorporateTestflightClient/Data/TicketsCacheActor.swift
@@ -17,13 +17,23 @@ actor TicketsCacheActor: TicketsRepository {
         print("Entering actor for \(key)")
         if let ticketTask = tickets[key] {
             print("Return cached value by \(key)")
-            return try await ticketTask.value
+            do {
+                let value = try await ticketTask.value
+                return value
+            } catch {
+                print("Previously cached task for the ticket \(key) returned error \(error). Will start a new task.")
+            }
         }
+        let newTicketTask = fetchTicketTask(key: key)
+        print("Returning value by fetching \(key)")
+        return try await newTicketTask.value
+    }
+
+    private func fetchTicketTask(key: String) -> Task<Ticket, Error> {
         print("Fetching value by \(key)")
         let ticketTask = Task { try await repository.getTicket(key: key) }
         print("Caching value by \(key)")
         tickets[key] = ticketTask
-        print("Return value by \(key)")
-        return try await ticketTask.value
+        return ticketTask
     }
 }
