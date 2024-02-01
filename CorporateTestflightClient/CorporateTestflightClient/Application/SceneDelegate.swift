1fd24bd5cb9e385e4cd2ed8f8c0ecad444afa481
Sergey Petrachkov <petrachkovsergey@gmail.com>
2024-02-01 10:00:29 +0100

    don't load UI if we're dealing with tests

--- CorporateTestflightClient/CorporateTestflightClient/Application/SceneDelegate.swift
+++ CorporateTestflightClient/CorporateTestflightClient/Application/SceneDelegate.swift
@@ -8,6 +8,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
 
+        if let testsAvailable = NSClassFromString("XCTestCase") {
+            return
+        }
+
         let navigationController = UINavigationController()
         let window = UIWindow(windowScene: windowScene)
         self.window = window
