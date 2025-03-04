58f1db9bc16617d7ce3868087491cef9a24acecc
Sergey Petrachkov <petrachkovsergey@gmail.com>
2025-03-04 16:08:48 +0100

    register image loader in the main app

--- CorporateTestflightClient/CorporateTestflightClient/Assembly/AppAssembly.swift
+++ CorporateTestflightClient/CorporateTestflightClient/Assembly/AppAssembly.swift
@@ -2,13 +2,22 @@ import CorporateTestflightDomain
 import Foundation
 import SimpleDI
 import TestflightNetworking
+import ImageLoader
 
 final class AppAssembly: Assembly {
 
 	func assemble(container: SimpleDI.Container) {
 
+
 		container.registerSingleton((any TestflightAPIProviding).self) { _,_ in
 			TestflightAPIProvider(session: .shared, decoder: JSONDecoder())
+		}
+
+		container.registerSingleton((any ImageLoader).self) { _,resolver in
+			guard let api = resolver.resolve(TestflightAPIProviding.self) else {
+				fatalError("My self-written DI sucks!")
+			}
+			return ImageCache(apiService: api)
 		}
 
 		container.register((any VersionsRepository).self) { _, resolver -> VersionsRepositoryImpl in
